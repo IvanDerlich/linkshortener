2a9e5b36db90eeb46198f9dd9144c8a145fb8c8b
Ivan Derlich <a@ivanderlich.com>
2024-10-23 09:55:48 -0300

    Fix short url format

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post("/", async (req, res) => {
     if (existingUrl) {
       return res.json({
         originalUrl,
-        shortUrl: `https://shorten.ivanderlich.com//${existingUrl.shortId}`,
+        shortUrl: `https://shorten.ivanderlich.com/${existingUrl.shortId}`,
       });
     }
     const shortId = nanoid(7); // Create a short ID with 7 characters
